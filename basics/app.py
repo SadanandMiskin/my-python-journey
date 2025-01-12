3f04b739ae7221ce74d34853687ac041e15ec40b
Sadanand Miskin <miskinsadanand@gmail.com>
2025-01-12 22:43:59 +0530

    Some Basics

---
+++ basics/app.py
@@ -0,0 +1,44 @@
+print("hello")
+
+
+for i in range(10):
+  print(i)
+
+
+# Liat: Similar to array
+my_list = [1, 2, "apple"]
+print(my_list)
+
+print(my_list.pop(1))
+print(my_list)
+
+# Tupples: Immutable arrays
+my_tupple = (1, 2, "apple")
+
+# Sets: Holds Unique elements
+my_set = {1,2,3,2} # Output: {1,2,3}
+print(my_set)   # In JS: const set  = new Set({1,2,3})
+
+# Dictionaries
+my_dict = {"name": "SadanandMiskin" , "age": 21}
+# In python the keys are string types where as in js keys are non-string types
+
+# Condition
+age = 18
+if age < 20:
+  print("Younger")
+elif age == 20:
+  print("20 Age")
+else:
+  print("Elder")
+
+
+
+# Loops
+for i in range(10):
+  print(i)
+
+i = 1
+while i == 10:
+  print(i)
+  i += 1
---
+++ basics/list.py
@@ -0,0 +1,7 @@
+# List
+items = ["apple" , "bananna" , "pineapple"]
+items.append("orange")  # ['apple', 'bananna', 'pineapple', 'orange']
+items.insert(1, "water") # ['apple', 'water', 'bananna', 'pineapple', 'orange']
+items.remove("bananna")   # ['apple', 'water', 'pineapple', 'orange']
+
+print(items)
